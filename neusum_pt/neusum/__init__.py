4ed4cfce4b0b37fc38540c96b5f4eb8db5e19966
removedporn <86824510+removedporn@users.noreply.github.com>
2023-07-08 18:11:31 +0800

    Delete neusum_pt directory

--- neusum_pt/neusum/__init__.py
+++
@@ -1,7 +0,0 @@
-import neusum.Constants
-import neusum.Models
-from neusum.Summarizer import Summarizer
-from neusum.Dataset import Dataset
-from neusum.Optim import Optim
-from neusum.Dict import Dict
-from neusum.Beam import Beam
--- neusum_pt/neusum/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-from neusum.modules.ConcatAttention import ConcatAttention
-from neusum.modules.Maxout import MaxOut
-from neusum.modules.myAdam import MyAdam
-from neusum.modules.ScoreAttention import ScoreAttention
